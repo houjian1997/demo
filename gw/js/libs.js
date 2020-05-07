1b29afd56fb04695485f709fe14c9d54c318daf9
tony <268282310@qq.com>
2020-05-07 18:41:52 +0800

    页面

--- gw/js/libs.js
+++ gw/js/libs.js
@@ -3,17 +3,17 @@
  * @param {routeList}  
  */
 export const initRoute = routeList => {
-    let str = "";
-    routeList.forEach(item => {
-        if (item.query) {
-            str += `<li><a href=${item.path}?${item.query} target=${item.target}>${item.name}</a></li>`
-        } else {
-            str += `<li><a href=${item.path} target=${item.target}>${item.name}</a></li>`
-        }
-    })
-    $(".route_ul").append(str);
-    let lastStr = $(".route_ul li:last a").html().substring(0, $(".route_ul li:last a").html().length - 8)
-    $(".route_ul li:last a").html(lastStr);
+  let str = "";
+  routeList.forEach(item => {
+    if (item.query) {
+      str += `<li><a href=${item.path}?${item.query} target=${item.target}>${item.name}</a></li>`
+    } else {
+      str += `<li><a href=${item.path} target=${item.target}>${item.name}</a></li>`
+    }
+  })
+  $(".route_ul").append(str);
+  let lastStr = $(".route_ul li:last a").html().substring(0, $(".route_ul li:last a").html().length - 8)
+  $(".route_ul li:last a").html(lastStr);
 }
 
 /**
@@ -21,19 +21,19 @@ export const initRoute = routeList => {
  * @param {index}  
  */
 export const handleChangeTab = index => {
-    $(".tab_ul li").each(function (index, item) {
-        $(item).removeClass("active")
-    })
-    $(".item_box").each(function (index, item) {
-        $(item).hide();
-    })
-    if (index != 0) {
-        $(".tab_ul li").eq(index - 1).addClass("active");
-        $(`#item_box_${index}`).show();
-    } else {
-        $(".tab_ul li").eq(0).addClass("active");
-        $(`#item_box_1`).show();
-    }
+  $(".tab_ul li").each(function (index, item) {
+    $(item).removeClass("active")
+  })
+  $(".item_box").each(function (index, item) {
+    $(item).hide();
+  })
+  if (index != 0) {
+    $(".tab_ul li").eq(index - 1).addClass("active");
+    $(`#item_box_${index}`).show();
+  } else {
+    $(".tab_ul li").eq(0).addClass("active");
+    $(`#item_box_1`).show();
+  }
 }
 
 
@@ -43,16 +43,16 @@ export const handleChangeTab = index => {
  * @param {tabIndex}  
  */
 export const createImgList = (imgList, tabIndex) => {
-    let str = "";
-    imgList.forEach(item => {
-        str += `<li><div><img src=${item.src} alt="" title=${item.title}></div>
+  let str = "";
+  imgList.forEach(item => {
+    str += `<li><div><img src=${item.src} alt="" title=${item.title}></div>
         <p>${item.title}</p></li>`
-    })
-    if (tabIndex != 0) {
-        $(`#item_box_${tabIndex} ul`).html(str)
-    } else {
-        $(`#item_box_1 ul`).html(str)
-    }
+  })
+  if (tabIndex != 0) {
+    $(`#item_box_${tabIndex} ul`).html(str)
+  } else {
+    $(`#item_box_1 ul`).html(str)
+  }
 }
 
 /**
@@ -63,17 +63,17 @@ export const createImgList = (imgList, tabIndex) => {
  * @param {tabIndex}
  */
 export const createPagination = (url, tabIndex, pageTotal, pageIndex) => {
-    let str = "";
-    str += `<li class="page-item disabled">
+  let str = "";
+  str += `<li class="page-item">
                <a class="page-link" href="${url}?tabIndex=${tabIndex}" tabindex="-1" aria-disabled="true">上一页</a>
             </li>`
-    for (var i = 0; i < pageTotal; i++) {
-        str += `<li class="page-item active" aria-current="page">
-               <a class="page-link" href="${url}?tabIndex=${tabIndex}">${i + 1}<span class="sr-only">(current)</span></a>
+  for (var i = 0; i < pageTotal; i++) {
+    str += `<li class="page-item" aria-current="page">
+               <a class="page-link" href="${url}?tabIndex=${tabIndex}">${i + 1}</a>
             </li>`
-    }
-    str += `<li class="page-item">
+  }
+  str += `<li class="page-item">
                 <a class="page-link" href="${url}?tabIndex=${tabIndex}">下一页</a>
             </li>`
-    return str;
+  return str;
 }
